95a97b39f100ecead544ab70aed6c7c894ad7de1
Leonardo Minora <leonardo.minora@gmail.com>
2023-04-26 19:43:24 -0300

    trocou retorno de str por um json

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      status: "ok",
+      message: "API Online",
+      description: "API exemplo da disciplina de POS"
+    };
   }
 }
